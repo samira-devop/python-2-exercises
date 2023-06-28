6001b7c1beb99481e87ca3b6dd86e8665122fdd1
Samira Boudjemai <sboudjemai@xpanxion.com>
2023-06-28 17:13:37 -0400

    ex1

--- src/solutions.py
+++ src/solutions.py
@@ -1,4 +1,8 @@
 from pprint import pprint
 
-def ex1():
-    pass #TODO:
+
+
+
+def sort_people(people_list, field, direction):
+
+    people_list.sort(key=lambda p: p[field], reverse= (direction == 'desc'))
